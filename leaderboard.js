a6254047e558dd128de3b5579eb275baa3e79b33
Adam Debbagh <adamdebbagh@gmail.com>
2015-12-10 08:07:15 -0500

    Added score functionality to players

--- leaderboard.js
+++ leaderboard.js
@@ -1,2 +1,36 @@
 PlayersList = new Mongo.Collection('players');
-//console.log("Hello world");
+
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    'player' : function () {
+    return PlayersList.find();
+    },
+    'selectedClass': function() {
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected";
+      }
+
+    }
+  });
+  Template.leaderboard.events({
+    'click .player': function() {
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    },
+    'click .increment': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update(selectedPlayer, {$inc:{score:5}});
+    },
+    'click .decrement': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update(selectedPlayer, {$inc:{score:-5}});
+    }
+  });
+  //console.log("Hello world");
+}
+
+if(Meteor.isServer){
+  console.log('Hello Adam');
+}
